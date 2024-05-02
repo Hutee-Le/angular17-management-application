65c2439ba3ad06188423ecc473cb99886860a0a3
Minh Nhut <lenhut0929@gmail.com>
2024-05-02 12:33:33 +0700

    Fetch data in UsersComponent

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  imports: [CommonModule],
+  // templateUrl: './users.component.html',
+  styleUrl: './users.component.css',
+  template: `
+					<ul>
+					  <li *ngFor="let item of items | async">
+						{{ item.name }}
+					  </li>
+					</ul>
+				  `
 })
 export class UsersComponent {
+  firestore: Firestore = inject(Firestore)
+  items: Observable<any[]>;
 
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items = collectionData(aCollection);
+  }
 }
