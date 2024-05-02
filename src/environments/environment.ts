994972389dd40ab2764baead44fa1f63dbba2121
Minh Nhut <lenhut0929@gmail.com>
2024-05-02 12:29:00 +0700

    add firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAC_OsTo-I6nDYtooGJ-TO3knF5C5x8Xu4',
+        authDomain: 'angular-management-application.firebaseapp.com',
+        projectId: 'angular-management-application',
+        storageBucket: 'angular-management-application.appspot.com',
+        messagingSenderId: '651929195211',
+        appId: '1:651929195211:web:6eef8e51864aba2f11c14e'
+    }
+};
