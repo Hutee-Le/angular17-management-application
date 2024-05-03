b096b71681d60e963467ee4a35dcd9a286b393c8
Minh Nhut <lenhut0929@gmail.com>
2024-05-03 12:16:19 +0700

    update product component

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  firestore: Firestore = inject(Firestore)
+  products: Observable<any[]>;
 
+  constructor() {
+    const aCollection = collection(this.firestore, 'products')
+    this.products = collectionData(aCollection);
+  }
 }
