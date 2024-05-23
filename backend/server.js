d10f57323843ae761a01bcff73eda117094795c4
Minh Nhut <lenhut0929@gmail.com>
2024-05-23 17:29:52 +0700

    setting up config file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
+// import necessary modules
 import express from 'express';
 import cors from 'cors';
 import { urlencoded, json } from 'body-parser';
 
-const app = express();
-app.use(urlencoded({ extended: false }));
-app.use(json());
-app.use(cors());
+import config from './config/config.js';
 
+// create express app
+const app = express();
+
+// Middleware setup
+app.use(urlencoded({ extended: false })); // parse URL-encoded data
+app.use(json()); // parse JSON data
+
+// config CORS
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions)); // enable CORS
+
+// define gET route
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "Hello World",
@@ -14,7 +28,8 @@ app.get("/", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+// start server
+const PORT = config.port || 3000;
 app.listen(PORT, () => {
-  console.log("server up and running");
+  console.log(`Server  up and running in at http://localhost:${PORT}/`);
 });
